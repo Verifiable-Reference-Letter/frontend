047ef97a3837856c8993b8107d9211aa4f33f98a
Maxim Vezenov <mav417@lehigh.edu>
2020-04-13 12:20:43 -0400

    moved contract deployment into componentDidMount, still having problems

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BigNumber } from "bignumber.js";
 import { TutorialToken } from "./contract-types/TutorialToken"; // import is correct
 import React from 'react';
 import TutorialTokenContractData from './contract-data/TutorialToken.json';
+import BN from "bn.js";
 
 const Web3 = require('web3');
 export let web3: typeof Web3;
@@ -9,9 +10,12 @@ export let web3: typeof Web3;
 export const GAS_LIMIT_STANDARD = 6000000;
 export let accounts: string[];
 let web3Provider;
+let contract: any;
 
 export async function deployContract<T>(contractName: string, abi:any, code:any, ...args: any[]): Promise<T> {
   
+  //const networkId = await web3.eth.net.getId();
+  //const deployedAddress = abi.networks[networkId].address;
   const Contract = new web3.eth.Contract(abi);
   console.log("Contract1: ");
   console.log(Contract);
@@ -31,12 +35,41 @@ export async function deployContract<T>(contractName: string, abi:any, code:any,
 }
 
 export function deployTutorialToken(): Promise<TutorialToken> {
-    return deployContract<TutorialToken>("TutorialToken", TutorialTokenContractData.abi, TutorialTokenContractData.bytecode, 0);
+    var contract = deployContract<TutorialToken>("TutorialToken", TutorialTokenContractData.abi, TutorialTokenContractData.bytecode, 0);
+    console.log("Contract from innner deploy tutorial token method: " + contract);
+    return contract;
 }
 
+type MyProps = {};
+type MyState = {
+  numErcBeingTraded: number
+  //contract: object
+};
+class App extends React.Component<MyProps, MyState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      numErcBeingTraded: 0
+      //contract: {}
+    };
 
-class App extends React.Component {
-  render() {
+    //this.handleErcInputChange = this.handleErcInputChange.bind(this);
+  }
+
+  handleErcInputChange(event: any) {
+    this.setState({ 
+      numErcBeingTraded: event.target.value 
+    });
+    console.log("Num of ERC wanted to trade: " + this.state.numErcBeingTraded);
+    //var rate = this.state.contract.rate().call();
+    var numErc = new BN(this.state.numErcBeingTraded);
+    //var numTokens = rate.mul(numErc);
+    //console.log("Num of Tutorial Tokens you can receive: " + numTokens.toString());
+  }
+
+  async componentDidMount() {
+    // TODO: Do all this stuff once onComponentDidMount
+    // Same with all my async shit
     if (typeof web3 !== 'undefined') {
       web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
@@ -47,13 +80,22 @@ class App extends React.Component {
     }
     console.log(web3.currentProvider);
 
-    const contract = deployTutorialToken();
-    console.log("Contract2: ");
-    console.log(contract);
+    contract = await deployTutorialToken();
+    var contractMethods = contract._parent.methods;
+    console.dir(contract);
+    console.log("Contract deploy address: " + contract._parent.options.address);
+    //console.log("Contract called from method: " + contract);
+    //console.log("Contract calling methods.rate().call(): " + contract.methods.rate().call());
+    //console.log("Contract calling .methods: " + contract.rate().call());
+    console.log("Contract calling _parent methods: " + (await contract._parent.methods.rate().call()));
+  }
+
+  render() {  
+
     return (
     	<div>
         <h1><b><i>Send ETC for Tutorial Token</i></b></h1>
-	  		<p>Amount ETC <input /></p>
+	  		<p>Amount ETC <input value={this.state.numErcBeingTraded} onChange={e => this.handleErcInputChange(e) }/></p>
 	  		<button>Purchase</button>
 	  	</div>
     );
