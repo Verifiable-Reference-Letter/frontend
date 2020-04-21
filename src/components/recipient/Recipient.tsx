3ec500400f3c9e4acfa3eb5d24f8d4adc2b702b3
Brian Zhao <brianzhao24@gmail.com>
2020-04-21 13:37:23 -0400

    removed backend api, updated receiver to recipient

--- src/components/recipient/Recipient.tsx
+++ src/components/recipient/Recipient.tsx
@@ -1,7 +1,7 @@
 //import { BigNumber } from "bignumber.js";
 // import { TutorialToken } from "./contract-types/TutorialToken"; // import is correct
 import React from 'react';
-import './Receiver.css';
+import './Recipient.css';
 
 
 const Web3 = require('web3');
@@ -9,13 +9,13 @@ export let web3: typeof Web3;
 
 
 
-class Receiver extends React.Component {
+class Recipient extends React.Component {
   render() {
     return (
-    	<div className="receiver-wrap">
-        This is the component for the person writing the letter
+    	<div className="recipient-wrap">
+        This is the component for the person receiving the letter
 	  	</div>
     );
   }
 }
-export default Receiver;
+export default Recipient;
