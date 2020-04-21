858a608c267b4545eda610723a1e4f644735f773
stevenshi99 <sts321@lehigh.edu>
2020-04-21 11:14:23 -0700

    first commit

---
+++ src/components/receiver/lettersData.ts
@@ -0,0 +1,32 @@
+const lettersData = [
+  {
+    letter_id: 1,
+    writer: {
+      user_id: 101,
+      name: "Mary Poppins",
+      public_key: "0x314159265358979323"
+    },
+    requester: {
+      user_id: 102,
+      name: "Simba",
+      public_key: "0xabcdefghijklmnop"
+    },
+    letter_uploaded: false
+  },
+  {
+    letter_id: 2,
+    writer: {
+      user_id: 1,
+      name: "Mary Poppins",
+      public_key: "0x314159265358979323"
+    },
+    requester: {
+      user_id: 103,
+      name: "Curious George",
+      public_key: "0x142857142857142857"
+    },
+    letter_uploaded: false
+  }
+];
+
+export default lettersData;
