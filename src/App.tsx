f05ba2af6299278ed15c02575201be0eb8450ff5
stevenshi99 <sts321@lehigh.edu>
2020-04-21 12:50:57 -0700

    added Recipient UI functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { BigNumber } from "bignumber.js";
 import { TutorialToken } from "./contract-types/TutorialToken"; // import is correct
-import React from 'react';
-import TutorialTokenContractData from './contract-data/TutorialToken.json';
+import React from "react";
+import TutorialTokenContractData from "./contract-data/TutorialToken.json";
 import BN from "bn.js";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 
-import Writer from './components/writer/Writer';
-import Requestor from './components/requestor/Requestor';
-import Recipient from './components/recipient/Recipient';
+import Writer from "./components/writer/Writer";
+import Requestor from "./components/requestor/Requestor";
+import Recipient from "./components/recipient/Recipient";
 
 import Web3 from "web3";
 export let web3: Web3;
@@ -20,34 +20,42 @@ export let accounts: string[];
 let web3Provider;
 
 let contract: any;
-const ERC20_NETWORK = "https://services.jade.builders/core-geth/kotti/1.11.2"
-export async function deployContract<T>(contractName: string, abi: any, code: any, ...args: any[]): Promise<T> {
+const ERC20_NETWORK = "https://services.jade.builders/core-geth/kotti/1.11.2";
+export async function deployContract<T>(
+  contractName: string,
+  abi: any,
+  code: any,
+  ...args: any[]
+): Promise<T> {
   const Contract = new web3.eth.Contract(abi);
   console.log("Contract1: ");
   console.log(Contract);
   const accounts = await web3.eth.getAccounts();
-  console.log(accounts)
-  console.log("deploying contract now")
+  console.log(accounts);
+  console.log("deploying contract now");
 
   const contractResult = await Contract.deploy({ data: code }).send({
     from: accounts[0]
-  })
+  });
   return contractResult as any;
 }
 
-
 export async function deployTutorialToken(): Promise<TutorialToken> {
   console.log("Deploying Contract from innner deploy tutorial token method: ");
-  var contract = await deployContract<TutorialToken>("TutorialToken", TutorialTokenContractData.abi, TutorialTokenContractData.bytecode, 0);
+  var contract = await deployContract<TutorialToken>(
+    "TutorialToken",
+    TutorialTokenContractData.abi,
+    TutorialTokenContractData.bytecode,
+    0
+  );
   console.log("Contract from innner deploy tutorial token method: " + contract);
   return contract;
 }
 
-
 type MyProps = {};
 type MyState = {
-  numErcBeingTraded: number
-  contract: TutorialToken 
+  numErcBeingTraded: number;
+  contract: TutorialToken;
 };
 class App extends React.Component<MyProps, MyState> {
   constructor(props: any) {
@@ -61,8 +69,8 @@ class App extends React.Component<MyProps, MyState> {
   }
 
   handleErcInputChange(event: any) {
-    this.setState({ 
-      numErcBeingTraded: event.target.value,
+    this.setState({
+      numErcBeingTraded: event.target.value
     });
     console.log("Num of ERC wanted to trade: " + this.state.numErcBeingTraded);
     var rate = this.state.contract.methods.rate().call();
@@ -74,26 +82,26 @@ class App extends React.Component<MyProps, MyState> {
   async componentDidMount() {
     // TODO: Do all this stuff once onComponentDidMount
     // Same with all my async shit
-    const ethereum = (window as any).ethereum
+    const ethereum = (window as any).ethereum;
     //TODO Ethereum enable may still be necessary
     // await ethereum.enable()
-    web3Provider =  (window as any).web3.currentProvider;
+    web3Provider = (window as any).web3.currentProvider;
     // NOTE you might need this
     //await ethereum.send('eth_requestAccounts')
 
     web3 = new Web3(web3Provider);
 
-    const accounts = await web3.eth.getAccounts()
-    console.log(accounts)
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
 
     contract = await deployTutorialToken();
 
-    this.setState({contract})
+    this.setState({ contract });
   }
 
-  render() { 
+  render() {
     return (
-    	<>
+      <>
         <Container fluid>
           <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="#home">ETC Reference Letter dApp</Navbar.Brand>
@@ -110,16 +118,27 @@ class App extends React.Component<MyProps, MyState> {
 	  		    <button>Purchase</button>
           </Row> */}
           <Row noGutters>
-            <Col><Requestor /></Col>
-            <Col><Writer user_id={1} name="Mary Poppins" public_key="0x314159265358979323"/></Col>
-            <Col><Recipient /></Col>
+            <Col>
+              <Requestor />
+            </Col>
+            <Col>
+              <Writer
+                user_id={101}
+                name="Mary Poppins"
+                public_key="0x314159265358979323"
+              />
+            </Col>
+            <Col>
+              <Recipient
+                user_id={103}
+                name="Curious George"
+                public_key="0x142857142857142857"
+              />
+            </Col>
           </Row>
         </Container>
-       
-	  	</>
+      </>
     );
   }
 }
 export default App;
-
-
--- src/components/recipient/Recipient.tsx
+++ src/components/recipient/Recipient.tsx
@@ -1,20 +1,102 @@
 //import { BigNumber } from "bignumber.js";
 // import { TutorialToken } from "./contract-types/TutorialToken"; // import is correct
-import React from 'react';
-import './Recipient.css';
+import React from "react";
+import User from "../../interfaces/User.interface";
+import Letter from "../../interfaces/Letter.interface";
+import "./Recipient.css";
 
-
-const Web3 = require('web3');
+const Web3 = require("web3");
 export let web3: typeof Web3;
 
+interface RecipientState {
+  letters: Letter[];
+}
 
+class Recipient extends React.Component<User, RecipientState> {
+  componentWillMount() {
+    // api call to get letters
+    this.setState({
+      letters: [
+        {
+          letter_id: 1,
+          writer: {
+            user_id: 101,
+            name: "Mary Poppins",
+            public_key: "0x314159265358979323"
+          },
+          requester: {
+            user_id: 102,
+            name: "Simba",
+            public_key: "0xabcdefghijklmnop"
+          },
+          letter_uploaded: false
+        },
+        {
+          letter_id: 2,
+          writer: {
+            user_id: 101,
+            name: "Mary Poppins",
+            public_key: "0x314159265358979323"
+          },
+          requester: {
+            user_id: 102,
+            name: "Simba",
+            public_key: "0xabcdefghijklmnop"
+          },
+          letter_uploaded: false
+        }
+      ]
+    });
+  }
 
-class Recipient extends React.Component {
+  constructor(props: User) {
+    super(props);
+    this.state = {
+      letters: []
+    };
+  }
+
   render() {
+    const { name, public_key, user_id } = this.props;
+    const { letters } = this.state;
+
+    const lettersList = letters.map((l, key) => (
+      <div key={l.letter_id}>
+        <p>
+          <span>({l.letter_id})&nbsp;</span>
+          <span>For: {l.requester.name} </span>
+          <button
+            style={{ marginLeft: "10px", float: "right" }}
+            // onClick={(e) => {
+            //   this.onViewClick(e, l.letter_id);
+            // }}
+          >
+            view
+          </button>
+          <span className="from">From: {l.writer.name}</span>
+        </p>
+      </div>
+    ));
     return (
-    	<div className="recipient-wrap">
-        This is the component for the person receiving the letter
-	  	</div>
+      <div className="recipient">
+        <div className="recipient-header">
+          <h1> Recipient Page </h1>
+          <p>
+            <em>{name}</em>
+          </p>
+          <hr></hr>
+        </div>
+
+        <div className="letters">
+          <h3> Letters </h3>
+          <div>{lettersList}</div>
+          <hr></hr>
+        </div>
+
+        <div className="recipient-footer">
+          <p> Product of Team Gas</p>
+        </div>
+      </div>
     );
   }
 }
