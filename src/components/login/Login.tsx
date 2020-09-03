732cbbdc7662c6bd7208a742f003e2849679da50
BZchu <brianzhao24@gmail.com>
2020-09-03 15:10:14 -0400

    minor changes to login

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Web3 from "web3";
-import detectEthereumProvider from '@metamask/detect-provider';
+import User from "../../interfaces/User.interface";
+import detectEthereumProvider from "@metamask/detect-provider";
 
 let web3: Web3;
-
 interface LoginProps {}
 interface LoginState {
   publicAddress: string;
   nonce: string;
 }
 
-class Login extends React.Component<LoginProps, LoginState> {
-  constructor(props: LoginProps) {
+class Login extends React.Component<User, LoginState> {
+  constructor(props: User) {
     super(props);
     this.state = {
       publicAddress: "",
@@ -20,9 +20,10 @@ class Login extends React.Component<LoginProps, LoginState> {
   }
 
   onLoginClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    console.log("Login clicked.");
     
-    // const publicAddress = web3.eth.coinbase.toLowerCase();
-    const publicAddress = "";
+    const publicAddress = this.props.public_key;
+    console.log(publicAddress);
 
     fetch(
       `${process.env.REACT_APP_BACKEND_URL}/users?publicAddress=${publicAddress}`
@@ -30,16 +31,17 @@ class Login extends React.Component<LoginProps, LoginState> {
       .then((response) => response.json())
       // If yes, retrieve it. If no, create it.
       .then((users) =>
-        users.length ? users[0] : this.handleSignup({ publicAddress })
+        users.length ? users[0] : this.signup({ publicAddress })
       )
       // Popup MetaMask confirmation modal to sign message
       .then(this.signMessage)
       // Send signature to back end on the /auth route
       .then(this.authenticate);
-    // --snip--
+
+    return 
   }
 
-  handleSignup({ publicAddress }: { publicAddress: string }) {
+  signup({ publicAddress }: { publicAddress: string }) {
     return fetch(`${process.env.REACT_APP_BACKEND_URL}/users`, {
       body: JSON.stringify(this.state.publicAddress),
       headers: {
@@ -55,20 +57,17 @@ class Login extends React.Component<LoginProps, LoginState> {
   }: {
     publicAddress: string;
     nonce: string;
-  }): Promise<{publicAddress: string, signature: any}> {
+  }): Promise<{ publicAddress: string; signature: any }> {
     return new Promise((resolve, reject) => {
-      // web3.personal.sign(
-      //   web3.fromUtf8(`I am signing my one-time nonce: ${nonce}`),
-      //   publicAddress,
-      //   (err, signature) => {
-      //     if (err) return reject(err);
-      //     return resolve({ publicAddress, signature });
-      //   }
-      // )
-      let signature = "";
-      return resolve({publicAddress, signature});
-      }
-    );
+      web3.eth.sign(
+        web3.utils.fromUtf8(`I am signing my one-time nonce: ${nonce}`),
+        publicAddress,
+        (err, signature) => {
+          if (err) return reject(err);
+          return resolve({ publicAddress, signature });
+        }
+      )
+    });
   }
 
   authenticate({
